9593f6f60339b35f2ad7f2fbcee3fa7499c22266
leobueno20 <109116969+leobueno20@users.noreply.github.com>
2024-03-06 14:38:29 -0300

    final?

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-// function customAlert(message) {
-//     document.getElementById("customAlertMessage").textContent = message;
-//     document.getElementById("customAlertBox").style.display = "block";
-// }
+function customAlert(message) {
+    document.getElementById("customAlertMessage").textContent = message;
+    document.getElementById("customAlertBox").style.display = "block";
+}
 
-// function closeCustomAlert() {
-//     document.getElementById("customAlertBox").style.display = "none";
-// }
+function closeCustomAlert() {
+    document.getElementById("customAlertBox").style.display = "none";
+}
